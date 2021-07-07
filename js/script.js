cf3250a9ba27f00558dcf583849aba220f53985a
Alexandria-dr <sasha110druz@gmail.com>
2021-07-07 22:18:48 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const navButton = document.querySelector(".nav__burger-button");
+const burgerMenu = document.querySelector(".nav__burger-menu")
+
+navButton.addEventListener("click", ()=>{
+    navButton.classList.toggle("active");
+    burgerMenu.classList.toggle("active");
+    document.querySelector("nav").classList.toggle("active")
+    document.querySelector("body").classList.toggle("lock");
+    document.querySelector(".nav__left-content").classList.toggle("active")
+})
