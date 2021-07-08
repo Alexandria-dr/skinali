4b969db9813f383d59914acb2516b0ee238f8c70
Alexandria-dr <sasha110druz@gmail.com>
2021-07-08 15:26:22 +0300

    features

--- js/script.js
+++ js/script.js
@@ -8,3 +8,32 @@ navButton.addEventListener("click", ()=>{
     document.querySelector("body").classList.toggle("lock");
     document.querySelector(".nav__left-content").classList.toggle("active")
 })
+
+const menuLinks = document.querySelectorAll(".menu-link[data-goto]");
+if (menuLinks.length > 0) {
+  menuLinks.forEach((menuLink) => {
+      
+    menuLink.addEventListener("click", onMenuLinkClick);
+  });
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        pageYOffset -
+        document.querySelector("nav").offsetHeight;
+        navButton.click();
+
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth",
+      });
+      e.preventDefault();
+    }
+  }
+}
